d65b64ff18dd16bb9d8b1b4d04a5fa84af0e4475
Boran <boranlearn@gmail.com>
2023-08-19 21:37:12 +0300

    GOARG IS WORKİNG

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/cetinboran/gosec
 
 go 1.20
 
-require github.com/cetinboran/goarg v0.0.0-20230819155520-86031a36158e
+require github.com/cetinboran/goarg v0.0.0-20230819181932-39eb8d104c48
--- main.go
+++ main.go
@@ -9,8 +9,18 @@ import (
 func main() {
 	Setup := cla.Init()
 	Setup.SetTitle("GoSec")
-	Setup.AddOption("-p,--password", false, "Enter your password.", []string{""})
+	Setup.AddOption("-p,--password", false, "Enter your password.", []string{"TOO LOW"})
+
+	Generate := cla.ModInit()
+	Generate.SetTitle("Generate Mode")
+	Generate.SetExamples([]string{"Example 1", "Example 2"})
+	Generate.AddOption("--generate", true, "Generate Password!", []string{"This is error!"})
+
+	Setup.AddMode("generate", &Generate)
+
 	Setup.AutomaticUsage()
+	Generate.AutomaticUsage()
+
 	args := Setup.Start()
 
 	fmt.Printf("args: %v\n", args)
