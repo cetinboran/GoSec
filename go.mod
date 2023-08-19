18d796abcf50e71e843f9704bc03b6b09a3f0746
Boran <boranlearn@gmail.com>
2023-08-20 01:32:49 +0300

    Login,Register,Config and encoding added.

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/cetinboran/gosec
 
 go 1.20
 
-require github.com/cetinboran/goarg v0.0.0-20230819184257-f4de8e10f4a2
+require github.com/cetinboran/goarg v1.0.0
--- main.go
+++ main.go
@@ -7,16 +7,36 @@ import (
 )
 
 func main() {
-	// goarg modlar kullanırken hangi moddan bu inputun geldiğini söyleyebilir.
+	// Setup ana main onda option şuanlık olmicak belki ilerde olur.
+	// Ama ana düşüncem o sadece basıldığında modların helpini göstersin veya modları göstersin
+
 	Setup := cla.Init()
 	Setup.SetTitle("GoSec")
 
+	// Login olunca true olsun jsonda ve onu kaydet. true ise diğer komutarı çalıştırmasına izin ver. logout atınca false yap çalıştıramasın başkası.
 	// Login Mode
 	Login := cla.ModInit()
 	Login.SetTitle("Login Mode")
 	Login.SetExamples([]string{"Example 1", "Example 2"})
-	Login.AddOption("-user,--username", false, "Enter Your Username", []string{"Wrong password or username"})
+	Login.AddOption("-u,--user", false, "Enter Your Username", []string{"Wrong Username"})
+	Login.AddOption("-p, --pass", false, "Enter Your Password", []string{"Wrong Password"})
 	Login.AutomaticUsage()
+
+	// Register Mode
+	Register := cla.ModInit()
+	Register.SetTitle("Register Mode")
+	Register.SetExamples([]string{"Example 1", "Example 2"})
+	Register.AddOption("-u, --user", false, "Enter Your Username", []string{"Wrong Username"})
+	Register.AddOption("-p, --pass", false, "Enter Your Password", []string{"Wrong Password"})
+	Register.AddOption("-cp, --cpass", false, "Confirm Your Password", []string{"Passwords do not match!"})
+	Register.AddOption("-s, --secret", false, "Enter Your Secret", []string{"Key Length Must Be 16,24 or 32!"})
+	Register.AutomaticUsage()
+
+	// Config Mode
+	Config := cla.ModInit()
+	Config.SetTitle("Config Mode")
+	Config.SetExamples([]string{"Example 1", "Example 2"})
+	Config.AddOption("-k, --key", false, "Enter The Secret Key's Key", []string{"Key Length Must Be 16,24 or 32!"})
 
 	// Key Mode
 	Key := cla.ModInit()
@@ -26,8 +46,9 @@ func main() {
 	Key.AutomaticUsage()
 
 	// Mode Init
-	Setup.AddMode("key", &Key)
 	Setup.AddMode("login", &Login)
+	Setup.AddMode("register", &Register)
+	Setup.AddMode("key", &Key)
 
 	// Setup.AutomaticUsage()
 
