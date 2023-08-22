bde179c3250a75d64e61506d04e4d35347ac17a9
Boran <boranlearn@gmail.com>
2023-08-22 19:53:16 +0300

    ...

--- register/handler.go
+++ register/handler.go
@@ -5,13 +5,11 @@ import (
 )
 
 func Start(args []structs.Input) {
-	// Üye olduğumuzda otomatik bir şekilde bu userId ye uygun bir config dosyası oluşmalı.
-
-	// Kayıt olan kişinin edineceği userId alttaki olucak. Normalde otomatik geliyor ancak bir config dosyası özel oluşaçağı için bize lazım.
-
 	newRegister := RegisterInit()
 	newRegister.TakeInput(args)
 	newRegister.CheckInputs()
 	newRegister.Save()
+
+	// Save attıktan sonra user için config dosyası oluşturduk.
 	newRegister.CreateConfig()
 }
