b992f462fa9ee167a826b04c3df6d2e866abde3c
Boran <boranlearn@gmail.com>
2023-08-25 23:59:27 +0300

    Key fixed

--- key/handler.go
+++ key/handler.go
@@ -1,20 +1,14 @@
 package key
 
 import (
-	"fmt"
-
 	cla "github.com/cetinboran/goarg/CLA"
 )
 
 // Start'ın her şeyin başladığı yer olsun.
 func Start(args []cla.Input) {
 	//global.Auth(args)
-
-	fmt.Println("Welcome to The Key Mode")
-
 	newKey := KeyInit()
 	newKey.TakeInput(args)
 	newKey.CheckInputs()
 	newKey.GenerateKey()
-
 }
